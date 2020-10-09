799a6eac8c64bc4d68917b547a0cd32b98124380
Gerardo Perez Soria <gerardoperezsoria@gmail.com>
2020-10-08 21:37:08 -0500

    feat: change lap top

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Cards from '../MDB/pages/CardsPage'
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -16,6 +19,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Cards />
   </Layout>
 )
 
